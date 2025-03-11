7d191f45b4b0b4a73e6fa7878ace5423f4f1f3fc
ilona <isirotinina@efinancialcareers.com>
2025-03-11 23:40:08 +0300

    add bsDatepicker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [BsDatepickerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
